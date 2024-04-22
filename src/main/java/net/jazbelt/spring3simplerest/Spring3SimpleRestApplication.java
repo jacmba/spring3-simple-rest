d67968df15fb8b54703ab41681615da0f0148034
Jacinto Mba Cantero <jacmba@gmail.com>
2024-04-22 13:07:06 +0100

    Initial commit

---
+++ src/main/java/net/jazbelt/spring3simplerest/Spring3SimpleRestApplication.java
@@ -0,0 +1,13 @@
+package net.jazbelt.spring3simplerest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring3SimpleRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring3SimpleRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/jazbelt/spring3simplerest/Spring3SimpleRestApplicationTests.java
@@ -0,0 +1,13 @@
+package net.jazbelt.spring3simplerest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring3SimpleRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
